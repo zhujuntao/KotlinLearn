93331485c1c35692dde7bda56cf2d7c83eb4bd7e
zhujuntao <zhujuntao@sinog2c.com>
2020-12-12 16:20:42 +0800

    添加新闻分页内容

---
+++ app/src/main/java/com/example/learn/fragment/NewsTitleFragment.kt
@@ -0,0 +1,4 @@
+package com.example.learn.fragment
+
+class NewsTitleFragment {
+}
---
+++ app/src/main/java/com/example/learn/ui/NewsMainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.learn.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewsMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news_main2)
+    }
+}
